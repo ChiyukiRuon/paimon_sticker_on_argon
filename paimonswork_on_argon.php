9c870312accf39c8a9e4b3617bd05105fc920a09
chiyukiruon <1275846107@qq.com>
2022-11-11 15:30:31 +0800

    添加注释

--- paimonswork_on_argon.php
+++ paimonswork_on_argon.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Plugin Name: paimon's work
+ * Plugin URI:  https://github.com/ChiyukiRuon/paimon_sticker_on_argon
+ * Text Domain: paimon_stickers_on_agron
+ * Description: 图源 米游社
+ * Version:     1.0
+ * Author:      ChiyukiRUon
+ * Author URI:  https://chiyukiruon.top
+ */
 function add_stickers($sticksList) {
     $sticks_dir = plugins_url('res/', __FILE__);
     $sticksList[] = array(
